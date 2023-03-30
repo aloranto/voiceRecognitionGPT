d10658474b1125fa41b4ad120f6b9a9b63f7f730
willson303 <alfonceep@gmail.com>
2023-03-30 03:32:16 +0200

    Se sube la primera funcionalidad del reconocimiento de voz

--- voiceRecognitionGPT.py
+++ voiceRecognitionGPT.py
@@ -1,31 +1,33 @@
+# Secret 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
+import os
+import openai
 import speech_recognition as sr
-import requests
+import tempfile
+
+os.getenv('OPENAI_API_KEY')
 
 # Crear un objeto de reconocimiento de voz
 r = sr.Recognizer()
 
 # Usar el micrófono como fuente de entrada de audio
 with sr.Microphone() as source:
-    
     # Ajustar el nivel de ruido de fondo
     r.adjust_for_ambient_noise(source)
-    
     # Indicar al usuario que comience a hablar
     print("Habla ahora:")
-    
     # Escuchar la entrada de audio del usuario
     audio = r.listen(source)
-    
-    # Enviar la entrada de audio a la API de ChatGPT
-    response = requests.post(
-        "https://api.openai.com/v1/engines/davinci-codex/completions", 
-        headers={"Authorization": "Bearer API_KEY"}, 
-        json={
-            "prompt": f"{r.recognize_google(audio)}", 
-            "max_tokens": 1000,
-            "temperature": 0.7
-        }
-    )
-    
-    # Imprimir la respuesta de la API
-    print(response.json()["choices"][0]["text"])
+
+    print("************   Se ha recogido: ", r.recognize_google(audio, language="es-ES"))
+
+    transcript = sr.Recognizer().recognize_google(audio, language="es-ES")
+    response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=transcript,
+    max_tokens=500,
+    n=1,
+    stop=None,
+    temperature=0.7,
+)
+print(response.choices[0].text)
+
